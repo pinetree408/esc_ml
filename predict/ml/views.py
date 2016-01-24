a55cfe884dfee3afd25d42e4356b121684eb79de
leesangyoon <pinetree408@gmail.com>
2016-01-24 17:12:12 +0900

    Add predict controll page html & css

--- predict/ml/views.py
+++ predict/ml/views.py
@@ -4,3 +4,9 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "ml/index.html"
+
+    def get_context_data(self,  **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['atomRange'] = range(0, 25)
+
+        return context

94da1c431c11bddd4fd0f9b599020a408fb3020c
leesangyoon <pinetree408@gmail.com>
2016-01-29 00:48:24 +0900

    Add Predicted data draw function

--- predict/ml/views.py
+++ predict/ml/views.py
@@ -9,7 +9,7 @@ class IndexView(TemplateView):
 
     def get_context_data(self,  **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        context['atomRange'] = range(0, 25)
+        context['atomRange'] = range(0, 72)
         context['form'] = self.form_class
 
         return context
@@ -17,9 +17,21 @@ class IndexView(TemplateView):
     def post(self, request, *args, **kwargs):
 	form = self.form_class(request.POST)
         context = {}
-	context['atomRange'] = range(0,25)
+	context['atomRange'] = range(0, 72)
         context['form'] = self.form_class
 
-	if form.is_valid():
-            context['result'] = predict.predict()
+        if form.is_valid():
+            predicted_data = predict.predict()
+            result_y = []
+            start = -10.0
+            while start <= 10.0:
+                result_y.append(start)
+                start = start + 0.02
+            final_result = []
+            for i in range(len(result_y)):
+                temp = []
+                temp.append(result_y[i])
+                temp.append(predicted_data[i])
+                final_result.append(temp)
+            context['result'] = final_result
         return render(request, self.template_name, context)
